2dad6af5c23d356559f88acdb862fa7510584269
Drew Norman <drewnorman@foxfuelcreative.com>
2021-11-24 18:22:09 -0700

    Adding configuration file reading and writing with initial interactive authorization setup

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-use clap::App as ClapApp;
+mod config;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+use anyhow::Result;
+use clap::{App as ClapApp, Arg};
+use config::Config;
+
+fn main() -> Result<()> {
     // Specify command-line app info
     let clap_app = ClapApp::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"));
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .help("Specify path to configuration file.")
+                .takes_value(true),
+        );
 
     // Parse provided command and parameters
-    clap_app.get_matches();
+    let matches = clap_app.get_matches();
+
+    // Load configuration
+    let mut config = Config::new();
+    config.load(matches.value_of("config"))?;
 
     // Success since no preceding errors
     Ok(())
