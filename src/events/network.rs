6730564a4cf4a939281e8c883b1fc35536a0c4d9
Drew Norman <drewnorman@foxfuelcreative.com>
2022-01-29 12:07:02 -0700

    Introducing my-tasks network event and reorganizing handle method

--- src/events/network.rs
+++ src/events/network.rs
@@ -10,6 +10,7 @@ use tokio::sync::Mutex;
 #[derive(Debug)]
 pub enum Event {
     Me,
+    MyTasks,
 }
 
 /// Specify struct for managing state with network events.
@@ -31,16 +32,37 @@ impl<'a> Handler<'a> {
     pub async fn handle(&mut self, event: Event) -> Result<()> {
         debug!("Processing network event '{:?}'...", event);
         match event {
-            Event::Me => {
-                let (user, workspaces) = self.asana.me().await?;
-                let mut state = self.state.lock().await;
-                state.set_user(user);
-                if !workspaces.is_empty() && state.get_active_workspace().is_none() {
-                    state.set_active_workspace(workspaces[0].gid.to_owned());
-                }
-                state.set_workspaces(workspaces);
-            }
+            Event::Me => self.me().await?,
+            Event::MyTasks => self.my_tasks().await?,
         }
+        Ok(())
+    }
+
+    /// Update state with user details.
+    ///
+    async fn me(&mut self) -> Result<()> {
+        let (user, workspaces) = self.asana.me().await?;
+        let mut state = self.state.lock().await;
+        state.set_user(user);
+        if !workspaces.is_empty() && state.get_active_workspace().is_none() {
+            state.set_active_workspace(workspaces[0].gid.to_owned());
+        }
+        state.set_workspaces(workspaces);
+        Ok(())
+    }
+
+    /// Update state with tasks assigned to the user.
+    ///
+    async fn my_tasks(&mut self) -> Result<()> {
+        let mut state = self.state.lock().await;
+        let my_tasks = self
+            .asana
+            .my_tasks(
+                &state.get_user().unwrap().gid,
+                &state.get_active_workspace().unwrap().gid,
+            )
+            .await?;
+        state.set_tasks(my_tasks);
         Ok(())
     }
 }
