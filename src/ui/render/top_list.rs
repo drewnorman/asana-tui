30ccf769650d7dca0bf45698dc6872568d3d1612
Drew Norman <drewnorman@foxfuelcreative.com>
2022-01-29 17:22:52 -0700

    Stying top list menu block based on current focus

--- src/ui/render/top_list.rs
+++ src/ui/render/top_list.rs
@@ -14,25 +14,22 @@ const BLOCK_TITLE: &str = "Projects";
 ///
 pub fn top_list(frame: &mut Frame, size: Rect, state: &State) {
     let mut block = Block::default().title(BLOCK_TITLE).borders(Borders::ALL);
-    if *state.current_menu() == Menu::TopList {
+    let list_item_style;
+    if *state.current_focus() == Focus::Menu && *state.current_menu() == Menu::TopList {
+        list_item_style = styling::active_list_item_style();
         block = block
             .border_style(styling::active_block_border_style())
             .title(Span::styled(
                 BLOCK_TITLE,
                 styling::active_block_title_style(),
             ));
+    } else {
+        list_item_style = styling::current_list_item_style();
     }
 
     if state.get_projects().len() == 0 {
         frame.render_widget(spinner::widget(state, size.height).block(block), size);
         return;
-    }
-
-    let list_item_style;
-    if *state.current_focus() == Focus::Menu && *state.current_menu() == Menu::TopList {
-        list_item_style = styling::active_list_item_style();
-    } else {
-        list_item_style = styling::current_list_item_style();
     }
 
     let items: Vec<Spans> = state
